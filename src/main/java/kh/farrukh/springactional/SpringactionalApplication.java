ed4143be866e7a3ba5767889e019510fe8a1d3ac
f_khusainov <f.khusainov@uzinfocom.uz>
2023-10-01 17:13:39 +0500

    Initial commit

---
+++ src/main/java/kh/farrukh/springactional/SpringactionalApplication.java
@@ -0,0 +1,13 @@
+package kh.farrukh.springactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringactionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringactionalApplication.class, args);
+	}
+
+}
