42b0ea9cdffdc86b461c8357cd61735b1e4dc263
f_khusainov <f.khusainov@uzinfocom.uz>
2023-10-02 12:01:18 +0500

    Added updateLastResult() method to manually update lastResult of Student

--- src/main/java/kh/farrukh/springactional/student/StudentRepository.java
+++ src/main/java/kh/farrukh/springactional/student/StudentRepository.java
@@ -1,6 +1,8 @@
 package kh.farrukh.springactional.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -8,4 +10,11 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 
   boolean existsByNameIgnoreCase(String name);
 
+  @Modifying
+  @Query("""
+      update Student student
+      set student.lastResult = :#{#result}
+      where student.id = :#{#id}
+      """)
+  void updateLastResult(Long id, Long result);
 }
--- src/main/java/kh/farrukh/springactional/student/StudentServiceImpl.java
+++ src/main/java/kh/farrukh/springactional/student/StudentServiceImpl.java
@@ -56,6 +56,7 @@ public class StudentServiceImpl implements StudentService {
 
     student.setLastResult(result);
     teacherService.changeBonusAccordingToExamResult(student.getTeacher().getId(), result);
+    studentRepository.updateLastResult(student.getId(), result);
 
     return new StudentExamResponseDto(student.getName(), result);
   }
